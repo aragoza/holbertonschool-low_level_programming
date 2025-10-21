cb42cdfd35ed292d07e07b59135cad5b9e984ec5
aragoza <12339@holbertonstudents.com>
2025-10-21 17:44:21 +0200

    count the number of bytes of a various types

---
+++ hello_world/6-size.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * printf - print what is between ()
+ * sizeof - is a function that count the nmbr of bytes in the type of the element
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+        printf(sizeof(char));
+        printf(sizeof(int));
+        printf(sizeof(long int));
+        printf(sizeof(long long int));
+        printf(sizeof(float));
+	return (0);
+}
