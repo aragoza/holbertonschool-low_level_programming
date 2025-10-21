b94aa42f9e0f03b1fca15953aaada547e8b4ddde
aragoza <12339@holbertonstudents.com>
2025-10-21 16:41:10 +0200

    is a program that print without printf

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,11 +1,7 @@
 #include <stdio.h>
 
-/**
- * main - Entry point
- *
- * Return: Always 0 (Success)
- */
-int puts(*"Programming is like building a multilingual puzzle)
+int main(void)
 {
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
