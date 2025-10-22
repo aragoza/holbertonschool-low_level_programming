4d5a2785be7fd8ee48f5fad001bd782a3e9c87c6
aragoza <12339@holbertonstudents.com>
2025-10-22 12:07:15 +0200

    print the size of some various type

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -2,17 +2,18 @@
 
 /**
  * main - Entry point
- * printf - print what is between ()
+ *
  * sizeof - count the nmbr of bytes in the type of the element
+ *
  * Return: Always 0 (Success)
  */
 
 int main(void)
 {
-	printf("Size of char: %zu byte(s)\n", sizeof(char));
-	printf("Size of int: %zu byte(s)\n", sizeof(int));
-	printf("Size of long int: %zu byte(s)\n", sizeof(long int));
-	printf("Size of long long: %zu byte(s)\n", sizeof(long long));
-	printf("Size of float: %zu byte(s)\n", sizeof(float));
+	printf("Size of char: %lu byte(s)\n", sizeof(char));
+	printf("Size of int: %lu byte(s)\n", sizeof(int));
+	printf("Size of long int: %lu byte(s)\n", sizeof(long int));
+	printf("Size of long long int: %lu byte(s)\n", sizeof(long long));
+	printf("Size of float: %lu byte(s)\n", sizeof(float));
 	return (0);
 }
