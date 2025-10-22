75c0377e372d4c7c95d456065f2a71bb74e066f3
aragoza <12339@holbertonstudents.com>
2025-10-22 16:06:36 +0200

    say if the nmbr is positive negative or zero

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -2,7 +2,7 @@
 #include <time.h>
 #include <stdio.h>
 
-/* main is the function that take in other function
+/* main is the function that take in other function*/
 /* using the condition if to make conditon to print different results*/
 
 int main(void)
