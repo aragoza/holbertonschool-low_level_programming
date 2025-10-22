e63b628ca0eeb05d4ccd6d4e775523dbdb6a63d7
aragoza <12339@holbertonstudents.com>
2025-10-22 09:42:36 +0200

    print the size of some various type

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-#include <libc6-dev-i386>
 
 /**
  * main - Entry point
@@ -7,11 +6,12 @@
  * sizeof - count the nmbr of bytes in the type of the element
  * Return: Always 0 (Success)
  */
+
 int main(void)
 {
-	printf("Size of int: %u bytes\n", sizeof(int));
-	printf("Size of double: %u bytes\n", sizeof(double));
-	printf("Size of float: %u bytes\n", sizeof(float));
-	printf("Size of char: %u bytes\n", sizeof(char));
+	printf("Size of int: %lu bytes\n", sizeof(int));
+	printf("Size of double: %lu bytes\n", sizeof(double));
+	printf("Size of float: %lu bytes\n", sizeof(float));
+	printf("Size of char: %lu bytes\n", sizeof(char));
 	return (0);
 }
