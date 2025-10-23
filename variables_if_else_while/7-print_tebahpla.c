f6a04e42014202f04efa1b407a202363a8ccf79a
aragoza <12339@holbertonstudents.com>
2025-10-24 00:53:25 +0200

    print the alphabet in reverse

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - main is a function that take in other function
+ *
+ * while - while is a function that loop if the condition ch is false
+ *
+ * Return: always (0)
+ */
+
+int main(void)
+{
+	char ch = 'z';
+
+	while (ch >= 'a')
+	{
+		putchar(ch);
+		ch--;
+	}
+
+	putchar('\n');
+
+	return (0);
+}
