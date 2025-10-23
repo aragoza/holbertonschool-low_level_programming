a1af1c9d6a6c5e18603a76ab4563ddb0fe0aac09
aragoza <12339@holbertonstudents.com>
2025-10-24 00:38:52 +0200

    print all single digit number in base 10 from 0 to 9

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - main is a function that take in other function
+ *
+ * while - while is a function that loop if the condition ch is false
+ *
+ * Return: always (0)
+ */
+
+int main(void)
+{
+	int i=0;
+
+	while (i < 10)
+	{
+		printf("%i\n", i);
+		i++;
+	}
+
+
+	return (0);
+}
