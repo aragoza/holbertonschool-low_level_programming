73248dba49dc82f22f5af1e9adbc9c23d36ad5f0
aragoza <12339@holbertonstudents.com>
2025-10-23 13:01:45 +0200

    it will print the alphabet and i choose to use a while loop to be able to modify it

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - main is a function that take in other function
+ *
+ * while - while is a function that loop if the condition ch is different from A (it will print the alphabet in lowercase
+ */
+
+int main(void)
+	{
+	char ch = 'a';
+	while(ch!='A')
+	{
+	putchar(ch);
+	ch++;
+	}
+	return(0);
+	}
