aaa76f9a8da6cf5bbe6e63598c1ee465d90e2e6a
aragoza <12339@holbertonstudents.com>
2025-10-23 17:15:22 +0200

    print upper and lower case alphabet

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+/**
+ * main - main is a function that take in other function
+ *
+ * while - while is a function that loop if the condition ch is false
+ *
+ * Return: always (0)
+ */
+
+int main(void)
+{
+
+	char ch = 'a';
+
+	while (ch != '{')
+	{
+		putchar(ch);
+		ch++;
+	}
+
+	char cha = 'A';
+
+	while (cha <= 'Z')
+	{
+		putchar(cha);
+		cha++;
+	}
+
+	putchar('\n');
+
+	return (0);
+}
