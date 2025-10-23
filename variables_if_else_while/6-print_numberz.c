2e1abae543708ad9b0f52c270fba4c806cc2bd95
aragoza <12339@holbertonstudents.com>
2025-10-24 00:50:49 +0200

    will print all single digit from 0 to 9 using a putchar to print the digit

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - main is a function that take in other function
+ *
+ * while - while is a function that loop if the condition ch is false
+ *
+ * Return: always (0)
+ */
+
+int main(void)
+{
+	int i = 0;
+
+	while (i < 10)
+	{
+		putchar('0' + i);
+		i++;
+	}
+
+	putchar('\n');
+
+
+	return (0);
+}
