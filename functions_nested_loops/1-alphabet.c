e6dfa12b09c824ba51b87db5c8a1fe5efe30e134
aragoza <charlet.elliot@gmail.com>
2025-10-26 15:14:59 +0100

    will print the alphabet with a simple function

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -1,15 +1,16 @@
 #include "main.h"
 
 /**
+ * main - take in the other function
  *
+ * print_alphabet - print the alphabet and is declare in main.h
  *
- *
- *
+ * Return: always (0)
  */
 
 int main(void)
 {
-	print_alphabet();
+	print_alphabet(); /** call the function describe in print_alphabet.c*/
 	_putchar('\n');
 
 	return (0);
---
+++ functions_nested_loops/print_alphabet.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_alphabet - definition of a function that print the alphabet
+ *
+ * putchar - print char 1 by 1
+ *
+ * no return because the function is define with void that is also a return in sort
+ */
+
+
+void print_alphabet()
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		_putchar(ch);
+	}
+}
