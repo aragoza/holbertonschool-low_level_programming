d7667606f55c696e9e09fe8105baaebb62d6ec6a
aragoza <12339@holbertonstudents.com>
2025-10-26 20:08:14 +0100

    while give the absolute value of a number n

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * _abs - give absolute value of n
+ *
+ * Return: (n) the distance when positive (-n) negative (0) when zero
+ *
+ * @n: only one parameter
+ */
+
+
+int _abs(int n)
+{
+
+	if (n >= 1)
+	{
+		return(n);
+	}
+	else if (n <= -1)
+	{
+		return(-n);
+	}
+	else
+	{
+		return (0);
+	}
+}
--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -15,6 +15,6 @@ int _isalpha(int c);
 
 int print_sign(int n);
 
-
+int _abs(int);
 
 #endif
