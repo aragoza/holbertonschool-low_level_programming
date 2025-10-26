cfff7fee2eccc27cb5400c027246a0d2f3557af8
aragoza <charlet.elliot@gmail.com>
2025-10-26 22:31:34 +0100

    print alll the hour in a day

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -32,24 +32,25 @@ void jack_bauer(void)
 		}
 	}
 	for (dizh = '2'; dizh <= '2'; dizh++)
-        {
-                for (unih = '0'; unih <= '3'; unih++)
-                {
-                        for (dizmin = '0'; dizmin <= '5'; dizmin++)
-                        {
-                                for (unimin = '0'; unimin <= '9'; unimin++)
-                                {
-                                        _putchar(dizh);
-                                        _putchar(unih);
-                                        _putchar(':');
-                                        _putchar(dizmin);
-                                        _putchar(unimin);
-                                        if ((dizh != '2') | (unih != '4') | (dizmin != '5') | (unimin != '9'))
-                                        {
-                                                _putchar('\n');
-                                        }
-                                }
-                        }
-                }
-        }
+	{
+		for (unih = '0'; unih <= '3'; unih++)
+		{
+			for (dizmin = '0'; dizmin <= '5'; dizmin++)
+			{
+				for (unimin = '0'; unimin <= '9'; unimin++)
+				{
+					_putchar(dizh);
+					_putchar(unih);
+					_putchar(':');
+					_putchar(dizmin);
+					_putchar(unimin);
+
+					if ((dizh != '2') | (unih != '4') | (dizmin != '5') | (unimin != '9'))
+					{
+						_putchar('\n');
+					}
+				}
+			}
+		}
+	}
 }
