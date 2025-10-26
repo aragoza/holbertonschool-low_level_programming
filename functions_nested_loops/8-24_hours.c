cf821853f9cfe27cbac39764f2706edc7f6dffc7
aragoza <12339@holbertonstudents.com>
2025-10-26 21:25:31 +0100

    print all possible time in a day

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -8,24 +8,25 @@
 
 void jack_bauer(void)
 {
-	int dizh;
-	int unih;
-	int dizmin;
-	int unimin;
+	char dizh;
+	char unih;
+	char dizmin;
+	char unimin;
 
-	for (dizh = 0; dizh <= 2; dizh++)
+	for (dizh = '0'; dizh <= '2'; dizh++)
 	{
-		for (unih = 0; unih <= 4; unih++)
+		for (unih = '0'; unih <= '4'; unih++)
 		{
-			for (dizmin = 0; dizmin <= 5; dizmin++)
+			for (dizmin = '0'; dizmin <= '5'; dizmin++)
 			{
-				for (unimin = 0; unimin <= 9; unimin++)
+				for (unimin = '0'; unimin <= '9'; unimin++)
 				{
 					_putchar(dizh);
 					_putchar(unih);
+					_putchar(':');
 					_putchar(dizmin);
 					_putchar(unimin);
-					if ((dizh != 2) | (unih != 4) | (dizmin != 5) | (unimin != 9))
+					if ((dizh != '2') | (unih != '4') | (dizmin != '5') | (unimin != '9'))
 					{
 						_putchar('\n');
 					}
