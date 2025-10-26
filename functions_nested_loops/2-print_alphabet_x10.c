7630c6c8f538dcb6a4772a9bc3cf376d28b412ee
aragoza <charlet.elliot@gmail.com>
2025-10-26 16:20:55 +0100

    will print all the alphabet 10 time

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_alphabet - print the alphabet
+ *
+ * no return because of void for the output
+ */
+
+void print_alphabet_x10(void)
+{
+	char c;
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (c = 'a'; c <= 'z'; c++)
+		{
+			_putchar(c);
+		}
+		if (i < 9)
+		{
+			_putchar('\n');
+		}
+	}
+	_putchar('\n');
+}
