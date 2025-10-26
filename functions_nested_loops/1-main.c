9108900f30e1af4154dbe253b4c6b43cc4469f68
aragoza <charlet.elliot@gmail.com>
2025-10-26 16:05:13 +0100

    repare the confusion in 1-main.c

--- functions_nested_loops/1-main.c
+++ functions_nested_loops/1-main.c
@@ -1,10 +1,11 @@
 #include "main.h"
 
 /**
- * main - check the code
+ * main - check the print_alphabet function
  *
- * Return: Always 0.
+ * Return: Always 0
  */
+
 int main(void)
 {
     print_alphabet();
