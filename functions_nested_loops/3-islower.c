0c3e685028f77f319b16ed19e9f6b7c0ca616c41
aragoza <charlet.elliot@gmail.com>
2025-10-26 17:44:21 +0100

    says if c is un lowercase with a return

--- functions_nested_loops/3-islower.c
+++ functions_nested_loops/3-islower.c
@@ -3,6 +3,8 @@
 /**
  * _islower - function that says if c i in lowercase
  *
+ * c is an int parameter that check the char by translating in int with ascii table
+ *
  * Return: (1) means c is in lowercase (0) if not
  */
 
