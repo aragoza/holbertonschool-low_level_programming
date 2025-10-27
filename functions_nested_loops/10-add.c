15692a2ed6cc9dcbf85c29b9e185365e047325d5
aragoza <12339@holbertonstudents.com>
2025-10-27 10:23:52 +0100

    will add two numbers and return the result

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * int add(int, int) - add two numbers
+ *
+ * Return: the addition n3
+ */
+
+int add(int n1, int n2)
+{
+	int n3 = (n1 + n2);
+	return (n3);
+
+}
--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -23,4 +23,8 @@ void jack_bauer(void);
 
 void times_table(void);
 
+int add(int, int);
+
+
+
 #endif
