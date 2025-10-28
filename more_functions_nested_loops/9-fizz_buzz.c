6568f566543dc301b59994f560dcd7dcdeb6fc12
aragoza <12339@holbertonstudents.com>
2025-10-28 13:27:34 +0100

    fix the FizzBuzz wasn't appearing

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -14,21 +14,25 @@ int main(void)
 
 	for (n = 1; n <= 100; n++)
 	{
-		if (n % 3 == 0)
+		if ((n % 3 ==0) && (n % 5 == 0))
 		{
-			printf("Fizz");
-		}
-		else if (n % 5 == 0)
-		{
-			printf("Buzz");
-		}
-		else if ((n % 3 ==0) && (n % 5 == 0))
-		{
-			printf("Fizz Buzz");
+			printf("FizzBuzz");
 		}
 		else
 		{
-			printf("%d", n);
+			if (n % 3 == 0)
+			{
+				printf("Fizz");
+			}
+			else if (n % 5 == 0)
+			{
+				printf("Buzz");
+			}
+
+			else
+			{
+				printf("%d", n);
+			}
 		}
 		printf(" ");
 	}
