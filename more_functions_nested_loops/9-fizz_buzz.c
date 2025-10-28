197c987f35ef35e1fd046043316508933e3cde1d
aragoza <12339@holbertonstudents.com>
2025-10-28 13:35:14 +0100

    fix the last space that doesn't exist anymore

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -3,9 +3,7 @@
 /**
  * main - print fizz if n|5 buzz if n|3 fizz buzz if n|5 and n|3
  *
- * @n: int that go from 0 to 100
- *
- * no return because of void output
+ * Return: always 0 (success)
  */
 
 int main(void)
