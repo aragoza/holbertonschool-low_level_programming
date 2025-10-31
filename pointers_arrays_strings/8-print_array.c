c3b5f1ae7661885c86eb6d64f8d870bb0c4b6069
aragoza <12339@holbertonstudents.com>
2025-10-31 09:27:25 +0100

    change the check of adress by adress for n and not further

--- pointers_arrays_strings/8-print_array.c
+++ pointers_arrays_strings/8-print_array.c
@@ -14,13 +14,13 @@
 void print_array(int *a, int n)
 {
 	int i = 0;
-
-	while (*(a + i) != '\0')
+	while (i < n)
 	{
 		printf("%d", *(a + i));
-		if (i < n - 1)
+
+		if (i < (n - 1))
 		{
-				printf(", ");
+			printf(", ");
 		}
 		i++;
 	}
