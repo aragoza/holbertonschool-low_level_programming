d1fa3738e49fceeb3d8482716ed8d6ce62d6596b
aragoza <charlet.elliot@gmail.com>
2025-11-02 14:55:07 +0100

    Write a function that convert a string to an integer.

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -1,58 +1,37 @@
 #include "main.h"
 
+/**
+ * _atoi - change type of a char into an int (output the first number in the string)
+ *
+ * @s: string
+ *
+ * Return: (0) if no digit in the string (n) the number in the string
+ */
 
 int _atoi(char *s)
 {
-	int i; /** check the size of the loop */
-	int countingplus = 0; /** count + sign */
-	int countingminus = 0; /** count - sign */
-	int signnumber; /** take the sign of the number */
-	int check0 = 0; /** check if digit exist in string */
-	int size_of_number = 1; /** check the size of the number in string */
-	int pow = 1; /** will permit the power of every digit in the final number */
-	int n; /** the return value number in the string */
+	int i = 0;
+	int sign = 1; /** siggn of the number */
+	int n = 0; /** ending result */
+	int started = 0; /** check for the beeginning of a number */
 
-	for (i = 0; *(s + i) != '\0'; i++) /** count + and - in string */
+	while (*(s + i) != '\0')
 	{
-		if (*(s + i) == '+')
+		if (!started && (*(s + i) == '-' || *(s + i) == '+'))
 		{
-			countingplus++;
+			if (*(s + i) == '-')
+			sign = -sign;
 		}
-		else if (*(s + i) == '-')
+		else if (*(s + i) >= '0' && *(s + i) <= '9')
 		{
-			countingminus++;
+			started = 1; /** there is at least 1 digit */
+			n = n * 10 + (*(s + i) - '0'); /** multiple then add the last digit */
 		}
+		else if (started != 0) /** is check only if the other 'if' are not tested */
+			break;
 
-		if ((*(s + i + 1) >= '0') && (*(s + i) <= '9')) /** calcul size of the number in string */
-		{
-			size_of_number++;
-			pow = pow * 10;
-			check0 = 1; /** if there is digit in the string */
-		}
-	}
-	if (check0 == 0) /** check if no digit in the string */
-	{
-		return (0);
+	i++;
 	}
 
-	signnumber = (countingplus % 2) - (countingminus % 2); /** set the sign of the number that will be return*/
-
-	if (signnumber >= 0)
-	{
-		signnumber = 1;
-	}
-	else
-	{
-		signnumber = (-1);
-	}
-
-	/** donne à n la valeur du nombre qui est dans la string*/
-	for (i = 0; (((*(s + i) >= '0') && (*(s + i) <= '9') && (size_of_number >= 0)); i++)
-	{
-		n += ((*(s + i) - 48) * pow);
-		size_of_number--;
-		pow = pow / 10;
-	}
-
-	return (signnumber * n);
+	return (n * sign);
 }
