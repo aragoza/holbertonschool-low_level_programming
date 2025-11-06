b24faec1f522aeb8acb68f68cd96ef1a61d24159
aragoza <12339@holbertonstudents.com>
2025-11-06 17:08:47 +0100

    Write a function that returns the natural square root of a number.

--- recursion/5-sqrt_recursion.c
+++ recursion/5-sqrt_recursion.c
@@ -1,13 +1,14 @@
 #include "main.h"
 
 /**
- * _sqrt_recursion - make square root of n
+ * stat_i - search for i the sqrt of n
+ *
+ * @i: the end sqrt of n
  *
  * @n: int will be square root
  *
- * Return: (i) the square root (-1) if there is no sqrt or n negative and 0 for 0
+ * Return: i the sqrt of n or -1
  */
-
 
 int stat_i(int n, int i)
 {
@@ -20,6 +21,13 @@ int stat_i(int n, int i)
 	return (stat_i(n, i + 1));
 }
 
+/**
+ * _sqrt_recursion - make square root of n
+ *
+ * @n: the number we have to sqaure root
+ *
+ * Return: (i) the square root (-1) no sqrt or n negative
+ */
 
 
 int _sqrt_recursion(int n)
--- recursion/main.h
+++ recursion/main.h
@@ -25,6 +25,7 @@ void times_table(void);
 
 int add(int, int);
 
+/** Slt Elliot, cv? */
 void print_to_98(int n);
 
 int _isupper(int c);
