e4c8a2ba151aff58a15fa0f1747bf242322d7786
aragoza <12339@holbertonstudents.com>
2025-11-12 13:29:37 +0100

    append the test if there is a problem when malloc

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -16,11 +16,11 @@ char *create_array(unsigned int size, char c)
 	unsigned int i = 0;
 	char *ar = malloc(size * sizeof(char));
 
-	if (size <= 0)
+	if (size == 0)
 		return (NULL);
 
 	if (ar == NULL)
-		free (ar);
+		return (NULL);
 
 	for(i = 0; i < size; i++)
 	{
