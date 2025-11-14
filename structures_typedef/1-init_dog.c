1ef0d01f120010af8b22f9ab391eaa658cc1f5a0
aragoza <12339@holbertonstudents.com>
2025-11-14 14:13:32 +0100

    append a test if d is NULL or if a string start with a null

--- structures_typedef/1-init_dog.c
+++ structures_typedef/1-init_dog.c
@@ -13,11 +13,14 @@
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
+	if (d == NULL)
+		d = malloc(sizeof(struct dog));
+	if (*name == '\0' || *owner == '\0')
+		return;
 
-	d->name = name;
-	d->age = age;
-	d->owner = owner;
-
+	(*d).name = name;
+	(*d).age = age;
+	(*d).owner = owner;
 }
 
 
