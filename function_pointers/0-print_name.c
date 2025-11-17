7d1122d7153292d6dc1a1abcb0ee929609ba51dd
aragoza <charlet.elliot@gmail.com>
2025-11-17 16:30:51 +0100

    print name

---
+++ function_pointers/0-print_name.c
@@ -0,0 +1,16 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - print the name using a function pointer
+ *
+ * @name: pointer to a string
+ * @f: pointer to a function that print name
+ *
+ * no return void
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+
+	f(name);
+}
