3d585120cba02a1c9c3e71f928223f509563975b
aragoza <12339@holbertonstudents.com>
2025-11-21 09:36:58 +0100

    append good doc and good style for betty

--- function_pointers/3-get_op_func.c
+++ function_pointers/3-get_op_func.c
@@ -5,29 +5,29 @@
  *
  * @s: char pointer to the good operator that will be checked
  *
- * Return: ops[i].f a pointer to the function in ops struct 
+ * Return: ops[i].f a pointer to the function in ops struct
  */
 
 int (*get_op_func(char *s))(int, int)
 {
- op_t ops[] = {
-    {"+", op_add},
-    {"-", op_sub},
-    {"*", op_mul},
-    {"/", op_div},
-    {"%", op_mod},
-    {NULL, NULL}
-};
- 
-    int i = 0;
+	op_t ops[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
+	};
 
-    while (ops[i].op != NULL)
-    {
-        if ((ops[i].op[0]) == s[0] && s[1] == '\0')
-        {
-            return (ops[i].f);
-        }
-        i++;
-    }
-    return (NULL);
+	int i = 0;
+
+	while (ops[i].op != NULL)
+	{
+		if ((ops[i].op[0]) == s[0] && s[1] == '\0')
+		{
+			return (ops[i].f);
+		}
+		i++;
+	}
+	return (NULL);
 }
