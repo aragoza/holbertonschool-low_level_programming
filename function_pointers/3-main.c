b48c9e05866510d2a4d0d70580ac9304159088ad
aragoza <12339@holbertonstudents.com>
2025-11-21 10:19:07 +0100

    add good style

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -3,31 +3,31 @@
 
 int main(int argc, char *argv[])
 {
-    int (*resultat)(int, int);
-    int num1, num2;
-    
-    if (argc != 4)
-    {
-        printf("Error\n");
-        exit(98);
-    }    
+	int (*resultat)(int, int);
+	int num1, num2;
 
-    resultat = get_op_func(argv[2]);
-    if (resultat == NULL)
-    {
-        printf("Error\n");
-        exit(99);
-    }
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
 
-    num1 = atoi(argv[1]);
-    num2 = atoi(argv[3]);
+	resultat = get_op_func(argv[2]);
+	if (resultat == NULL)
+	{
+		printf("Error\n");
+		exit(99);
+	}
 
-    if ((argv[2][0] == '/' || argv[2][0] == '%') && (num2 == 0))
-    { 
-        printf("Error\n");
-        exit(100);
-    }
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[3]);
 
-    printf("%d\n", resultat(num1, num2));
-    return (0);
+	if ((argv[2][0] == '/' || argv[2][0] == '%') && (num2 == 0))
+	{
+		printf("Error\n");
+		exit(100);
+	}
+
+	printf("%d\n", resultat(num1, num2));
+	return (0);
 }
