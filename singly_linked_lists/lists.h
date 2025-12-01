2f3e23434deb41043dade69ac950e1c50929fd7f
aragoza <12339@holbertonstudents.com>
2025-12-01 15:22:45 +0100

    header that hold the macros and structures

---
+++ singly_linked_lists/lists.h
@@ -0,0 +1,17 @@
+#ifndef _LISTS_H_
+#define _LISTS_H_
+
+#include <stddef.h>
+
+typedef struct list_s
+{
+    char *str;
+    unsigned int len;
+    struct list_s *next;
+} list_t;
+
+size_t print_list(const list_t *h);
+
+int _putchar(char c);
+
+#endif /** _LISTS_H_ */
