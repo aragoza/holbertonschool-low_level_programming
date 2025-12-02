9e225730e8913fbb375569cefc537be219d3e31a
aragoza <12339@holbertonstudents.com>
2025-12-02 15:11:52 +0100

    write a function that free the memory of a singly linked list

---
+++ singly_linked_lists/4-free_list.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * free_list - will free every value stock in every chunck of the list h
+ *
+ * @head: list of type list_t 
+ *
+ * Return: void
+ */
+
+void free_list(list_t *head)
+{
+    list_t *free_head;
+
+    while(head != NULL)
+    {
+        free_head = head->next;
+        head = head->next;
+        free(free_head);
+    }
+
+}
