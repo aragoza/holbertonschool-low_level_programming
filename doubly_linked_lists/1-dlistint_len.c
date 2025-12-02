89f439b1ae08143fecec98906993d4d115096fb7
aragoza <12339@holbertonstudents.com>
2025-12-02 15:48:18 +0100

    wrie a function that count the number of node in a doubly linked list

---
+++ doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,23 @@
+    #include "lists.h"
+#include <stdio.h>
+
+/**
+ * print_list - will print every value stock in every chunck of the list h
+ *
+ * @h: const list of type list_t 
+ *
+ * Return: (number_of_node) the number of node
+ */
+
+size_t dlistint_len(const dlistint_t *h)
+{
+	unsigned int number_of_node = 0;
+
+	while(h != NULL)
+	{
+		number_of_node++;
+		h = h->next;
+	}
+
+	return (number_of_node);
+}
