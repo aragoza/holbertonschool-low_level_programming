0d4b4efbbf86531981f3996cbfd9cf8c61f75ccd
aragoza <12339@holbertonstudents.com>
2025-12-03 12:31:42 +0100

    write a function that insert a node a precise index and return the node

---
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -0,0 +1,56 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ *  get_dnodeint_at_index - return the value of the node at a given index
+ *
+ * @h: list of type dlistint_t
+ * @idx: index of the node we want 
+ *
+ * Return: (new_node) the node at the index
+ */
+
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
+{
+	unsigned int number_of_node;
+	dlistint_t *new_node, *temp;
+
+	number_of_node = 0;
+	new_node = malloc(sizeof(dlistint_t));
+	if (new_node == NULL)
+	{
+		printf("Error\n");
+		return (NULL);
+	}
+
+	/** new_node = {n ,NULL, NULL}; */
+
+	new_node->n = n;
+	new_node->prev = NULL;
+	new_node->next = NULL;
+
+	if (*h == NULL)
+	{
+		*h = new_node;
+		return (new_node);
+	}
+
+	temp = *h;
+
+	while(number_of_node < idx - 1)
+	{
+		if (temp == NULL)
+			return (NULL);
+
+		number_of_node++;
+		temp = temp->next;
+	}
+
+	new_node->next = temp->next;
+	new_node->prev = temp;
+	temp->next = new_node;
+	temp = temp->next->next;
+	temp->prev = new_node;
+
+	return (new_node);
+}
