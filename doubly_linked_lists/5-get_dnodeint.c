8f331138eaf74551cd44621be9be16e9d6bfb255
aragoza <12339@holbertonstudents.com>
2025-12-03 17:47:34 +0100

    add a better style and doc

--- doubly_linked_lists/5-get_dnodeint.c
+++ doubly_linked_lists/5-get_dnodeint.c
@@ -2,10 +2,10 @@
 #include <stdio.h>
 
 /**
- *  get_dnodeint_at_index - return the value of the node at a given index
+ * get_dnodeint_at_index - return the value of the node at a given index
  *
  * @head: list of type dlistint_t
- * @index: index of the node we want 
+ * @index: index of the node we want
  *
  * Return: (head) the node at the index
  */
@@ -14,12 +14,12 @@ dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {
 	unsigned int number_of_node = 0;
 
-	while(number_of_node < index)
+	while (number_of_node < index)
 	{
 		if (head == NULL)
-            return (NULL);
+			return (NULL);
 
-        number_of_node++;
+		number_of_node++;
 		head = head->next;
 	}
 
