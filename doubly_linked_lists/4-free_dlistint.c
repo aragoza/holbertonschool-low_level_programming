d1853e1850e74646ada1501dd187c1e5c0a831f5
aragoza <12339@holbertonstudents.com>
2025-12-03 10:23:53 +0100

    write a function that free the memory allocated to every node in linked list

---
+++ doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * free_list - will free every value stock in every chunck of the list h
+ *
+ * @head: list of type list_t 
+ *
+ * Return: void
+ */
+
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *free_head;
+
+	while(head != NULL)
+	{
+		free_head = head->next;
+		head = head->next;
+		free(free_head);
+	}
+
+}
