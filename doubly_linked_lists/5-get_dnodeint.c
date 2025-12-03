32de75e386b32c9c5d5f1916ba95ee3ff8eb8f11
aragoza <12339@holbertonstudents.com>
2025-12-03 10:38:20 +0100

    return the node at the given index in the linked list

---
+++ doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ *  get_dnodeint_at_index - return the value of the node at a given index
+ *
+ * @head: list of type dlistint_t
+ * @index: index of the node we want 
+ *
+ * Return: (head) the node at the index
+ */
+
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+	unsigned int number_of_node = 0;
+
+	while(number_of_node < index)
+	{
+		if (head == NULL)
+            return (NULL);
+
+        number_of_node++;
+		head = head->next;
+	}
+
+	return (head);
+}
