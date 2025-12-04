156fca61c920101d6b4b036d02f78160dfa7f013
aragoza <12339@holbertonstudents.com>
2025-12-04 10:57:06 +0100

    modify the condition to change the prev of the new node if head is NULL or head is non NULL

--- doubly_linked_lists/2-add_dnodeint.c
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -18,14 +18,20 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	new_node = malloc(sizeof(dlistint_t));
 	if (new_node == NULL)
 	{
-		printf("Error\n");
 		return (NULL);
 	}
 
 	new_node->n = n;
-	new_node->prev = NULL;
-	new_node->next = *head;
+	if (*head == NULL)
+	{
+		new_node->prev = NULL;
+	}
+	else
+	{
+		(*head)->prev = new_node;
+	}
 
+	new_node->next = *head;
 	*head = new_node;
 
 	return (new_node);
