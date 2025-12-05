df02873ebe584678c9e9c013232da314db36d3b8
aragoza <12339@holbertonstudents.com>
2025-12-05 19:10:41 +0100

    Write a program that copies the content of a file to another file.

---
+++ file_io/3-cp.c
@@ -0,0 +1,79 @@
+#include <fcntl.h>
+#include <unistd.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ */
+
+int main(int argc, char *argv[])
+{
+	int filde_from, filde_to, r, w;
+	char *buf;
+
+	if (argc != 3)
+	{
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit(97);
+	}
+
+	buf = malloc(1024);
+	if (buf == NULL)
+	{
+		return (-1);
+	}
+
+	filde_from = open(argv[1], O_RDONLY);
+	if (filde_from == -1)
+	{
+		dprintf(STDERR_FILENO, "Can't read from file %s", argv[1]);
+		exit(98);
+	}
+
+	filde_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
+	if (filde_to == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s", argv[2]);
+		exit(99);
+	}
+
+	if ((r = read(filde_from, buf, 1024)) > 0)
+	{
+		w = write(filde_to, buf, r);
+		if (r != w)
+			{
+				dprintf(STDERR_FILENO, "Error: Can't write to %s", argv[2]);
+				exit(99);
+			}	
+	}
+
+	if (r == -1)
+	{
+		dprintf(STDERR_FILENO, "Can't read from file %s", argv[1]);
+		exit(98);	
+	}
+
+	if (close(filde_from) == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d", filde_from);
+		exit(100);
+	}
+
+		if (close(filde_to) == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d", filde_to);
+		exit(100);
+	}
+
+	free(buf);
+	return (0);
+}
